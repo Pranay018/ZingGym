f5644a1b52845ccfdc519cd864fa4f5855fa3f33
Pranay Patankar <pranaypatankar005@gmail.com>
2025-11-11 13:55:57 +0530

    quick fixes

--- frontend/src/components/Testimonial.jsx
+++ frontend/src/components/Testimonial.jsx
@@ -7,30 +7,36 @@ const reviews = [
     name: "Pranay",
     username: "@pranay_.18._",
     body: "Zing Gym changed my lifestyle completely. Trainers are super friendly!",
+    loading: "lazy",
+    
   },
   {
     img: "/images/testimonial2.webp",
     name: "Pradeep",
     username: "@pradeep_30",
     body: "Amazing vibe! I love the evening group classes and the energy here.",
+    loading: "lazy",
   },
   {
     img: "/images/testimonial3.webp",
     name: "Dhruv",
     username: "@_Dhruv04",
     body: "Best membership investment ever. The transformation is really amazing!",
+    loading: "lazy",
   },
   {
     img: "/images/testimonial4.webp",
     name: "Jatin",
     username: "@jatin_14",
     body: " After 8 weeks of abs training, I can see real definition and feel stronger than ever!",
+    loading: "lazy",
   },
   {
     img: "/images/testimonial5.webp",
     name: "Altaf",
     username: "@altaf01",
     body: "Love the ambience and staff support. Zing Gym keeps me motivated!",
+    loading: "lazy",
   },
 ];
 
@@ -40,6 +46,7 @@ const TestimonialsSection = () => {
       <h2 className="testimonials-title">What Our Members Say 💪</h2>
 
       {/* The marquee now runs outside of any card container box */}
+      <div className="marquee-wrapper">
       <div className="marquee">
         {[...reviews, ...reviews].map((review, index) => (
           <div className="testimonial-card" key={index}>
@@ -57,6 +64,7 @@ const TestimonialsSection = () => {
             <blockquote>“{review.body}”</blockquote>
           </div>
         ))}
+      </div>
       </div>
     </section>
   );
